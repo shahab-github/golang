b88188ed8e7eb1ec80e8fa51e888d4878e4c0a37
shahab-github <shahabuddinm679+github@gmail.com>
2024-12-25 19:54:36 +0530

    feat: updated the functions excercise with the solutions

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -3,23 +3,55 @@
 //  information to the terminal.
 //
 //--Requirements:
-//* Write a function that accepts a person's name as a function
-//  parameter and displays a greeting to that person.
-//* Write a function that returns any message, and call it from within
-//  fmt.Println()
-//* Write a function to add 3 numbers together, supplied as arguments, and
-//  return the answer
-//* Write a function that returns any number
-//* Write a function that returns any two numbers
-//* Add three numbers together using any combination of the existing functions.
-//  * Print the result
+
 //* Call every function at least once
 
 package main
 
 import "fmt"
 
-func main() {
-
+//   - Write a function that accepts a person's name as a function
+//     parameter and displays a greeting to that person.
+func greetPerson(name string) {
+	fmt.Println("Hello Mrs", name)
 }
 
+//   - Write a function that returns any message, and call it from within
+//     fmt.Println()
+func returnSomething() string {
+	return "Some good message"
+}
+
+//   - Write a function to add 3 numbers together, supplied as arguments, and
+//     return the answer
+func addThree(a, b, c int) int {
+	return a + b + c
+}
+
+// * Write a function that returns any number
+func five() int {
+	return 5
+}
+
+// * Write a function that returns any two numbers
+func twoTwos() (int, int) {
+	return 2, 2
+}
+
+func main() {
+	fmt.Println(returnSomething())
+
+	greetPerson("John")
+
+	//* Add three numbers together using any combination of the existing functions.
+	//  * Print the result
+	a, b := twoTwos()
+	fivevalue := five()
+
+	sum := addThree(a, b, fivevalue)
+	fmt.Println(sum)
+
+	newSum := addThree(five(), a, b)
+	fmt.Println(newSum)
+
+}
