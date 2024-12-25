abcce6be4353cf84199bd9f3cb3253c0d84722cd
shahab-github <shahabuddinm679+github@gmail.com>
2024-12-25 19:43:01 +0530

    feat: update the demo of the functions

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -2,7 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
-
+// double takes the inputs as an integer and double and return it
+func double(x int) int {
+	return x + x
 }
 
+// add takes two inputs and return the sum
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+// greet, it takes not input and return no output
+func greet() {
+	fmt.Println("Hello from the greet function !")
+}
+
+func main() {
+
+	//calling greet function
+	greet()
+
+	dozen := double(6)
+	fmt.Println("Dozen", dozen)
+
+	// bakerDozen
+	bakerDozen := add(dozen, 1)
+	fmt.Println("Baker Dozen", bakerDozen)
+
+	// another bakers dozen
+	anotherBakerDozen := add(double(6), 1)
+	fmt.Println("Another Baker Dozen", anotherBakerDozen)
+}
