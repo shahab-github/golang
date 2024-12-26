88d366a13d8731ad60d3d17deb7a6cb715507c28
shahab-github <shahabuddinm679+github@gmail.com>
2024-12-26 16:54:47 +0530

    feat: updatd the switch excercise

--- src/lectures/exercise/switch/switch.go
+++ src/lectures/exercise/switch/switch.go
@@ -14,5 +14,17 @@ package main
 import "fmt"
 
 func main() {
-}
 
+	switch age := 0; {
+	case age == 0:
+		fmt.Println("New Born")
+	case age >= 1 && age <= 3:
+		fmt.Println("toddler")
+	case age < 13:
+		fmt.Println("child")
+	case age < 18:
+		fmt.Println("teenager")
+	default:
+		fmt.Println("adult")
+	}
+}
