f5fc8e7e8afd296a8e4965cb901da70dbda613b6
shahab-github <shahabuddinm679+github@gmail.com>
2024-12-26 14:46:59 +0530

    feat: updatd the excercise if-else

--- src/lectures/exercise/if-else/if-else.go
+++ src/lectures/exercise/if-else/if-else.go
@@ -5,13 +5,6 @@
 //  needed to grant resource access using any combination of `if`, `else if`,
 //  and `else`.
 //
-//--Requirements:
-//* Use the accessGranted() and accessDenied() functions to display
-//  informational messages
-//* Access at any time: Admin, Manager
-//* Access weekends: Contractor
-//* Access weekdays: Member
-//* Access Mondays, Wednesdays, and Fridays: Guest
 
 package main
 
@@ -37,6 +30,10 @@ const (
 	Guest      = 50
 )
 
+func weekday(day int) bool {
+	return day <= 4
+}
+
 func accessGranted() {
 	fmt.Println("Granted")
 }
@@ -47,8 +44,24 @@ func accessDenied() {
 
 func main() {
 	// The day and role. Change these to check your work.
-	today, role := Tuesday, Guest
+	today, role := Wednesday, Guest
 
-	accessGranted()
+	//--Requirements:
+	//* Use the accessGranted() and accessDenied() functions to display
+	if role == Admin || role == Manager {
+		accessGranted()
+	} else if role == Contractor && !weekday(today) {
+		//* Access at any time: Admin, Manager
+		//* Access weekends: Contractor
+		accessGranted()
+	} else if role == Member && weekday(today) {
+		//* Access weekdays: Member
+		accessGranted()
+	} else if role == Guest && (today == Monday || today == Wednesday || today == Friday) {
+		//* Access Mondays, Wednesdays, and Fridays: Guest
+		accessGranted()
+	} else {
+		accessDenied()
+	}
+
 }
-
