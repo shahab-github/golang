959794c4ed00ff1e23e3667bcfc96ca225c1860e
shahab-github <shahabuddinm679+github@gmail.com>
2024-12-26 17:45:28 +0530

    feat: updated the loops excercise

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,5 +15,18 @@ package main
 import "fmt"
 
 func main() {
-}
+	for i := 1; i <= 50; i++ {
+		divisibleBy3 := i%3 == 0
+		divisibleBy5 := i%5 == 0
 
+		if divisibleBy3 && divisibleBy5 {
+			fmt.Println("FizzBuzz")
+		} else if divisibleBy3 {
+			fmt.Println("Fizz")
+		} else if divisibleBy5 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
