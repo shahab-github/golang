320b371fc1712f8cb9a9f6d0f1f67e1347323700
shahab-github <shahabuddinm679+github@gmail.com>
2024-12-26 15:35:43 +0530

    feat: updated the switch demo

--- src/lectures/demo/switch/switch.go
+++ src/lectures/demo/switch/switch.go
@@ -13,6 +13,25 @@ const (
 )
 
 func main() {
+	switch p := price(); {
+	case p < 2:
+		fmt.Println("cheap item")
+	case p < 10:
+		fmt.Println("moderately price item")
+	default:
+		fmt.Println("Expensive Item")
+	}
+
+	ticket := Economy
+	switch ticket {
+	case Economy:
+		fmt.Println("Economy seating")
+	case Business:
+		fmt.Println("Business seating")
+	case FirstClass:
+		fmt.Println("First class seating")
+	default:
+		fmt.Println("other")
+	}
 
 }
-
