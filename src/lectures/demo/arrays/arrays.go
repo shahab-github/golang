6b6500916cfa4f80aca2f961c751f3650ea5f9b9
shahab-github <shahabuddinm679+github@gmail.com>
2025-01-04 21:47:28 +0530

    feat: updated the demo for array

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,7 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Room struct {
+	name    string
+	cleaned bool
 }
 
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Printf("%s is cleaned\n", room.name)
+		} else {
+			fmt.Printf("%s is not cleaned\n", room.name)
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+	fmt.Println("Performing cleaning")
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanliness(rooms)
+}
