368464aff610f20961ab66298f2d04a724365395
shahab-github <shahabuddinm679+github@gmail.com>
2025-01-05 16:01:52 +0530

    feat: updated the demo for range

--- src/lectures/demo/ranges/ranges.go
+++ src/lectures/demo/ranges/ranges.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-}
+	slice := []string{"Hello", "World", "!"}
 
+	for i, elem := range slice {
+		fmt.Println(i, elem)
+
+		for _, ch := range elem {
+			fmt.Printf("   %q\n", ch)
+		}
+	}
+}
