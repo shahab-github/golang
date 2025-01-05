805b7604d12e6c5e23d72b25ea89883fb6a1b99a
shahab-github <shahabuddinm679+github@gmail.com>
2025-01-05 15:52:42 +0530

    feat: updated the excercise for slice

--- src/lectures/exercise/slices/slices.go
+++ src/lectures/exercise/slices/slices.go
@@ -18,6 +18,28 @@ import "fmt"
 
 type Part string
 
-func main() {
+func showLine(line []Part) {
+	for i := 0; i < len(line); i++ {
+		part := line[i]
+		fmt.Println(part)
+	}
 }
 
+func main() {
+	assemblyLine := make([]Part, 3)
+
+	assemblyLine[0] = "Pipe"
+	assemblyLine[1] = "Bolt"
+	assemblyLine[2] = "Sheet"
+
+	fmt.Println("3 parts:")
+	showLine(assemblyLine)
+
+	assemblyLine = append(assemblyLine, "Washer", "Wheel")
+	fmt.Println("\nAdded two parts:")
+	showLine(assemblyLine)
+
+	assemblyLine = assemblyLine[3:]
+	fmt.Println("\nSliced")
+	showLine(assemblyLine)
+}
